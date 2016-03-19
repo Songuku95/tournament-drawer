4621db80e1dd38e9a1c21b9f0742b77b61e76687
Duy Khanh <duykhanhuet@gmail.com>
2016-03-19 10:48:24 +0700

    Implement Functions Tournament

--- docs/tournament.md
+++ docs/tournament.md
@@ -32,21 +32,21 @@ tournament: {
 
 ##Functions
 
-- addGroup(title)
-- getGroup(group_index)
+- addGroup(title) => addGroup('abc')
+- getGroup(group_index) => getGroup(1)
 
-- addMatch(group_index, match)
-- removeMatch(match_id)
-- removeMatch(group_index, match_index)
-- updateMatch(match_id, match)
-- updateMatch(group_index, match_index, match)
-- getMatch(match_id)
-- getMatch(group_index, match_index)
+- addMatch(group_index, match) => addMatch(1, {id: 789, player1: 'sample_player', player2: '#32#win', winner: 1})
+- removeMatch(match_id) => removeMatchById(53)
+- removeMatch(group_index, match_index) => removeMatch(0,0)
+- updateMatch(match_id, match) => updateMatchById(123,{player1: 'changed123', id: 52})
+- updateMatch(group_index, match_index, match) => updateMatchById(123,{player1: 'changed123', id: 52})
+- getMatch(match_id) => getMatchById(52)
+- getMatch(group_index, match_index) => getMatch(0,10)
 
-- addPlayer(player)
-- addPlayer(players)
-- removePlayer(player)
-- removePlayer(player_index)
+- addPlayer(player) => addPlayer('123')
+- addPlayer(players) => addPlayers(['124','128'])
+- removePlayer(player) => removePlayerById('124')
+- removePlayer(player_index) => removePlayer(0)
 
 - html()
 - js()
--- index.js
+++ index.js
@@ -3,30 +3,104 @@
 module.exports = class Tournament {
 
   constructor() {
-    this.participants = [];
-    this.groups = [];
+    this.participants = []
+    this.groups = []
   }
 
   addGroup(title) {
     this.groups.push({
       title: title
     });
+    this.groups[this.groups.length - 1].matches = new Array();
   }
 
   removeGroup(index) {
-    if (typeof index != "number")
-      this.groups = this.groups.filter(e => e.title == index);
-    else
-      this.groups.splice(index, 1);
+    this.groups.splice(index, 1);
+  }
+
+  getGroup(group_index){
+    if (this.groups.length <= group_index)
+      return null;
+    return this.groups[group_index];
+  }
+
+  addMatch(group_index, match){
+    if (this.groups.length <= group_index) return;
+    this.groups[group_index].matches.push(match);
+  }
+
+  removeMatchById(match_id){
+    for (var i = 0; i < this.groups.length; i++){
+      for (var j = 0; j < this.groups[i].matches.length; j++)
+        if (this.groups[i].matches[j].id == match_id) {
+          this.groups[i].matches.splice(j,1);
+          return;
+        } 
+    }
+  }
+
+  removeMatch(group_index, match_index){
+    this.groups[group_index].matches.splice(match_index,1);
+  }
+
+
+  addPlayer(player){
+    this.participants.push(player);
+  }
+
+  addPlayers(players){
+    this.participants.push.apply(this.participants, players);
+  }
+
+  updateMatchById(match_id, match){
+   for (var i = 0; i < this.groups.length; i++){
+      for (var j = 0; j < this.groups[i].matches.length; j++)
+        if (this.groups[i].matches[j].id == match_id) {
+          this.updateMatch(i, j, match);
+          return;
+        } 
+    } 
+  }
+
+  updateMatch(group_index,match_index, match){
+    for (var i in match){
+      this.groups[group_index].matches[match_index][i] = match[i];
+    }
+  }
+
+  getMatchById(match_id){
+    for (var i = 0; i < this.groups.length; i++){
+      for (var j = 0; j < this.groups[i].matches.length; j++)
+        if (this.groups[i].matches[j].id == match_id) {
+          return this.groups[i].matches[j];
+        } 
+    }  
+    return null;
+  }
+
+  getMatch(group_index, match_index){
+    if (this.groups.length <= group_index)
+      return null;
+    if (this.groups[group_index].matches.length <= match_index)
+      return null;
+    return this.groups[group_index].matches[match_index];
+  }
+
+  removePlayerById(player){
+    for (var i = 0; i < this.participants.length; i++)
+      if (this.participants[i] == player){
+        this.participants.splice(i,1);
+        return
+      }
+  }
+
+  removePlayer(player_index){
+    this.participants.splice(player_index,1);
   }
 
   get html() {
     return "";
   }
 
-  clear() {
-    this.participants = [];
-    this.groups = [];
-  }
-
 }
+
